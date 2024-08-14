e0e501bb0e779f37a7e51563316909dd203a5bed
Tensorboy2 <svargdal@online.no>
2024-08-14 13:52:12 +0200

    new headder

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Tensorboy2's GitHub Page</h1>
-        <p>
-          This site is built with React and TypeScript!
-        </p>
+        <h1>Under construction.</h1>
+        <p>-Tensorboy2</p>
       </header>
     </div>
   );
